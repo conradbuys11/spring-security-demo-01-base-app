eb4713b59429304544c781255ad771c5eb9159f7
conradbuys11 <conradbuys11@gmail.com>
2022-03-02 15:50:57 -0500

    custom access denied page

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -25,11 +25,12 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/").hasRole("EMPLOYEE")
+			.antMatchers("/").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
 			.antMatchers("/leaders/**").hasRole("MANAGER")
 			.antMatchers("/systems/**").hasRole("ADMIN")
 			.and().formLogin().loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll()
-			.and().logout().permitAll();
+			.and().logout().permitAll()
+			.and().exceptionHandling().accessDeniedPage("/access-denied");
 	}
 	
 	
